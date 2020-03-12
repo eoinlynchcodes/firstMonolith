1267b24c7f1b627ac0b46c96abf5c54714534666
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-12 11:37:16 +0000

    yep

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+function getBaseURL() {
+  return process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : ''
+}
 function App() {
+  const [message, setMessage] = useState('')
+  useEffect(() => {
+    fetch(getBaseURL() + '/api')
+      .then(res => res.json())
+      .then(data => {
+        setMessage(data.message)
+      })
+  }, [])
   return (
-<h1>Hi there friend.</h1>
+    <div className="App">
+      <header className="App-header">
+        { message }
+      </header>
+    </div>
   );
 }
-
 export default App;
